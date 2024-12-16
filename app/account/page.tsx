bb09e728bdc03b653bf4bb36b5dee82c7694f7b5
kahalagan00 <privatelampas@gmail.com>
2024-12-16 08:22:34 -0800

    Started building over nested layouts of the app

--- app/account/page.tsx
+++ app/account/page.tsx
@@ -1,7 +1,13 @@
+export const metadata = {
+  title: "Guest area",
+};
+
 const page: React.FC = () => {
   return (
     <div>
-      <h1>Your Account</h1>
+      <h2 className="font-semibold text-2xl text-accent-400 mb-7">
+        Welcome, Jonas
+      </h2>
     </div>
   );
 };
